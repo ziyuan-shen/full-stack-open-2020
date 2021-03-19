b79ce7c950fc91b2858238b67941974be1e41e58
Ziyuan Shen <ziyuan.shen@duke.edu>
2021-03-19 01:02:54 -0700

    add ex2.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -26,33 +26,63 @@ const Total = ({ exercises }) => {
 
 const Course = ({ course }) => {
     return (
-        <>
-        <Header course={course}/>
-        <Content parts={course.parts}/>
-        <Total exercises={course.parts.map(course => course.exercises)}/>
-        </>
+        course.map(
+          course =>
+          <>
+          <Header course={course}/>
+          <Content parts={course.parts}/>
+          <Total exercises={course.parts.map(course => course.exercises)}/>
+          </>
+        )
     )
 }
   
 const App = () => {
   // const-definitions
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
+  const course = [
     {
-        name: 'Fundamentals of React',
-        exercises: 10
-    },
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
     {
-        name: 'Using props to pass data',
-        exercises: 7
-    },
-    {
-        name: 'State of a component',
-        exercises: 14
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
     }
-    ]
-  }
+  ]
 
   return (
     <Course course={course}/>
