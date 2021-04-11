82796405d817a4a036881f009a3dae287fff7d03
Ziyuan Shen <ziyuan.shen@duke.edu>
2021-04-11 01:22:54 -0700

    add ex3.2

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -27,6 +27,11 @@ let persons = [
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
+
+app.get('/info', (request, response) => {
+    response.send(`<div>Phonebook has info for ${persons.length} people</div>`
+        + `<div>${new Date()}</div>`)
+})
   
 const PORT = 3001
 app.listen(PORT, () => {
