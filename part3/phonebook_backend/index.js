448adbc8cb1aa14a7cf166c2176d094bc9d405f6
Ziyuan Shen <ziyuan.shen@duke.edu>
2021-04-11 23:39:26 -0700

    add ex3.9-3.11

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 app.use(morgan('tiny'))
+app.use(cors())
 
 let persons = [
     {
