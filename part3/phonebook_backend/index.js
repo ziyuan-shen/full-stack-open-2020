b886ee8096e2b3ca10bb551bb7fcdd428e065a00
Ziyuan Shen <ziyuan.shen@duke.edu>
2021-04-15 02:46:52 -0700

    add ex3.12

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const app = express()
 app.use(morgan('tiny'))
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     {
@@ -91,7 +92,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
